e2a5d3ed2dcc603684bc33eedb3fba02d317d947
Karol Olszański <olszanski@yahoo.com>
2019-10-23 23:02:44 +0200

    Initial Commit

---
+++ Fridge/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Fridge
+//
+//  Created by Karol Olszański on 23/10/2019.
+//  Copyright © 2019 Karol Olszański. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Fridge/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Fridge
+//
+//  Created by Karol Olszański on 23/10/2019.
+//  Copyright © 2019 Karol Olszański. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
